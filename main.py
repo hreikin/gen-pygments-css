f9da42c2f769ab7eab043e3b46a2cc4e1f628df6
Michael Haslam <hreikin@gmail.com>
2022-04-04 22:08:35 +0100

    Add css_selector to gen_pygments_css() function so CSS classes can be prepended onto the generated Pygments styles. Default is None.

--- main.py
+++ main.py
@@ -6,8 +6,15 @@ from pygments.styles import get_all_styles
 
 logging.basicConfig(level=logging.DEBUG)
 
-def gen_pygments_css(css_dir="css/", styles_list=list(get_all_styles())):
+def gen_pygments_css(css_dir="css/", styles_list=list(get_all_styles()), css_selector=None):
     """Generate `CSS` stylesheets for each `Pygments` supported style.
+    
+    CSS stylesheets for all `Pygments` styles are output into a created `css/` 
+    directory by default. 
+    
+    To generate a single stylesheet pass in a string containing the name of a 
+    `Pygments` style or to generate multilple stylesheets pass in a list of 
+    strings containing `Pygments` style names.
     
     Example Usage:
     
@@ -29,14 +36,10 @@ def gen_pygments_css(css_dir="css/", styles_list=list(get_all_styles())):
         # Create a list of strings containing the paths of all stylesheets.
         my_list = gen_pygments_css()
         
+        # Call with a CSS selector defined.
+        gen_pygments_css(css_selector=".highlight")
+        
         ```
-
-    CSS stylesheets for all `Pygments` styles are output into a created `css/` 
-    directory by default. 
-    
-    To generate a single stylesheet pass in a string containing the name of a 
-    `Pygments` style or to generate multilple stylesheets pass in a list of 
-    strings containing `Pygments` style names.
     
     Args:
         css_dir (str): Relative or absolute string of the location to output the 
@@ -45,6 +48,10 @@ def gen_pygments_css(css_dir="css/", styles_list=list(get_all_styles())):
             of `Pygments` styles. Defaults to a list of all style names returned 
             by the `get_all_styles()` function from `Pygments` 
             (https://pygments.org/docs/api/?highlight=get_all_styles#pygments.styles.get_all_styles).
+        css_selector (Union[str, list, tuple]): A string, list or tuple 
+            containing the CSS selector(s) to prefix onto the generated 
+            `Pygments` classes. More info available here: 
+            https://pygments.org/docs/formatters/?highlight=get_style_defs#HtmlFormatter
     
     Returns:
         file_list (list(str)): A list of strings containing the location(s) of 
@@ -65,7 +72,10 @@ def gen_pygments_css(css_dir="css/", styles_list=list(get_all_styles())):
         file_list.append(file_location)
         logging.info(f"Style: {item}")
         logging.info(f"Location: {file_location}")
-        subprocess.run([f"pygmentize -S {item} -f html -a .highlight > {file_location}"], shell=True)
+        if css_selector == None:
+            subprocess.run([f"pygmentize -S {item} -f html > {file_location}"], shell=True)
+        else:
+            subprocess.run([f"pygmentize -S {item} -f html -a {css_selector} > {file_location}"], shell=True)
     logging.info(f"Generated CSS stylesheets available at '{css_dir}'.")
     return file_list
 
