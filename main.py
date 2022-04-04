5be6d26beba2b87963791e1e25dc341302ef32df
Michael Haslam <hreikin@gmail.com>
2022-04-04 21:40:35 +0100

    Create a file list to return from the function. Update the docstring.

--- main.py
+++ main.py
@@ -8,6 +8,28 @@ logging.basicConfig(level=logging.DEBUG)
 
 def gen_pygments_css(css_dir="css/", styles_list=list(get_all_styles())):
     """Generate `CSS` stylesheets for each `Pygments` supported style.
+    
+    Example Usage:
+    
+        ```python
+        
+        # Called with no arguments.
+        gen_pygments_css()
+        
+        # Called with a string passed into the styles_list.
+        gen_pygments_css(styles_list="monokai")
+        
+        # Called with a list of strings passed into the styles_list.
+        gen_pygments_css(styles_list=["monokai", "stata-dark"])
+        
+        # Call with a custom css_dir, can be relative or absolute.
+        gen_pygments_css(css_dir="assets/styles/")
+        gen_pygments_css(css_dir="/home/user/project/assets/styles/")
+        
+        # Create a list of strings containing the paths of all stylesheets.
+        my_list = gen_pygments_css()
+        
+        ```
 
     CSS stylesheets for all `Pygments` styles are output into a created `css/` 
     directory by default. 
@@ -25,7 +47,8 @@ def gen_pygments_css(css_dir="css/", styles_list=list(get_all_styles())):
             (https://pygments.org/docs/api/?highlight=get_all_styles#pygments.styles.get_all_styles).
     
     Returns:
-        css_dir (Path): The location of the generated CSS stylesheet(s).
+        file_list (list(str)): A list of strings containing the location(s) of 
+            the generated CSS stylesheet(s).
     """
     if isinstance(styles_list, str):
         styles_str = styles_list
@@ -34,15 +57,17 @@ def gen_pygments_css(css_dir="css/", styles_list=list(get_all_styles())):
     css_dir = Path(css_dir).resolve()
     logging.info(f"Creating '{css_dir}' directory if it doesn't already exist.'")
     css_dir.mkdir(parents=True, exist_ok=True)
+    
+    file_list = list()
     logging.info("Generating CSS stylesheets.")
     for item in styles_list:
+        file_location = f"{css_dir.resolve()}/{item}.css"
+        file_list.append(file_location)
         logging.info(f"Style: {item}")
-        logging.info(f"Location: {css_dir.resolve()}/{item}.css")
-        subprocess.run([f"pygmentize -S {item} -f html > css/{item}.css"], shell=True)
+        logging.info(f"Location: {file_location}")
+        subprocess.run([f"pygmentize -S {item} -f html -a .highlight > {file_location}"], shell=True)
     logging.info(f"Generated CSS stylesheets available at '{css_dir}'.")
-    return css_dir
+    return file_list
 
 if __name__ == "__main__":
-    logging.debug("Logger initialized.")
-    # gen_pygments_css(styles_list="monokai")
     gen_pygments_css()
