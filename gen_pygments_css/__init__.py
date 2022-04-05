828b7e6224995077fab16bbacd89db3a68d55908
Michael Haslam <hreikin@gmail.com>
2022-04-05 13:44:03 +0100

    Update version numbers.

--- gen_pygments_css/__init__.py
+++ gen_pygments_css/__init__.py
@@ -31,4 +31,4 @@ logging.getLogger(__name__).addHandler(logging.NullHandler())
 # ################################################################################
 
 # Version of gen-pygments-css package
-__version__ = "0.0.1"
+__version__ = "0.0.2"
