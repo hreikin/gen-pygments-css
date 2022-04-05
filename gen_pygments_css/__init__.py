6881c68fed169fbaf1aa569bea7eb04975594049
Michael Haslam <hreikin@gmail.com>
2022-04-05 13:16:38 +0100

    Follow logging best practises for libraries and only import a NullHandler so logging can be configured by the user. Add a commented out basic logging configuration for development.

--- gen_pygments_css/__init__.py
+++ gen_pygments_css/__init__.py
@@ -1,2 +1,34 @@
+import logging
+
+# Follow logging best practises for libraries and only import a NullHandler so 
+# logging can be configured by the user.
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+# ##################################### LOGS #####################################
+# # Uncomment the below section when developing if logging is required.
+# ################################################################################
+# # Initialize the logger and specify the level of logging. This will log "DEBUG" 
+# # and higher messages to file and log "INFO" and higher messages to the console.
+# logging.basicConfig(level=logging.DEBUG,
+#                     format='%(asctime)s %(levelname)s: %(message)s',
+#                     datefmt='%d-%m-%y %H:%M:%S',
+#                     filename='debug.log',
+#                     filemode='w')
+
+# # Define a "handler" which writes "INFO" messages or higher to the "sys.stderr".
+# console = logging.StreamHandler()
+# console.setLevel(logging.INFO)
+
+# # Set a format which is simpler for console messages.
+# formatter = logging.Formatter('[%(asctime)s]: %(message)s', datefmt='%H:%M:%S')
+
+# # Tell the console "handler" to use this format.
+# console.setFormatter(formatter)
+
+# # Add the "handler" to the "root logger".
+# logging.getLogger('').addHandler(console)
+
+# ################################################################################
+
 # Version of gen-pygments-css package
 __version__ = "0.0.1"
